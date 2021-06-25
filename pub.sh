8aa7488b39b71a170b56019da627eb61620dd525
hychu@hpcargo <hychu@hpcargo>
2021-06-25 23:05:59 +0800

    nocomment

--- pub.sh
+++ pub.sh
@@ -1,4 +1,5 @@
-gfm
+git pull
+git fetch
 rm -rf build
 docker run --rm -it -v /root/github/nats.java:/opt/nats.java al-gradle bash -c "cd /opt/nats.java; gradle -x test build"
 docker build . --no-cache -t iscr.io:5002/nats-test
