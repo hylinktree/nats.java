9133af0b2f6b886a997f56b71248f76827981edd
朱玄義 <hsiuenyichu@insnergy.com>
2021-06-25 22:26:52 +0800

    nocomment

--- src/examples/java/io/nats/examples/jetstream/NatsJsPub.java
+++ src/examples/java/io/nats/examples/jetstream/NatsJsPub.java
@@ -28,36 +28,25 @@ import java.nio.charset.StandardCharsets;
  * This example will demonstrate JetStream publishing.
  */
 public class NatsJsPub {
-    static final String usageString =
-            "\nUsage: java -cp <classpath> NatsJsPub [-s server] [-strm stream] [-sub subject] [-mcnt msgCount] [-m messageWords+] [-r headerKey:headerValue]*"
-                    + "\n\nDefault Values:"
-                    + "\n   [-strm stream]    example-stream"
-                    + "\n   [-sub subject]    example-subject"
-                    + "\n   [-mcnt msgCount]  10"
-                    + "\n   [-m messageWords+] hello"
-                    + "\n   [-mn msgLength] 32"
-                    + "\n\nRun Notes:"
-                    + "\n   - msg_count < 1 is the same as 1"
-                    + "\n   - headers are optional"
-                    + "\n\nUse tls:// or opentls:// to require tls, via the Default SSLContext\n"
-                    + "\nSet the environment variable NATS_NKEY to use challenge response authentication by setting a file containing your private key.\n"
-                    + "\nSet the environment variable NATS_CREDS to use JWT/NKey authentication by setting a file containing your user creds.\n"
-                    + "\nUse the URL for user/pass/token authentication.\n";
+    static final String usageString = "\nUsage: java -cp <classpath> NatsJsPub [-s server] [-strm stream] [-sub subject] [-mcnt msgCount] [-m messageWords+] [-r headerKey:headerValue]*"
+            + "\n\nDefault Values:" + "\n   [-strm stream]    example-stream" + "\n   [-sub subject]    example-subject"
+            + "\n   [-mcnt msgCount]  10" + "\n   [-m messageWords+] hello" + "\n   [-mn msgLength] 32"
+            + "\n\nRun Notes:" + "\n   - msg_count < 1 is the same as 1" + "\n   - headers are optional"
+            + "\n\nUse tls:// or opentls:// to require tls, via the Default SSLContext\n"
+            + "\nSet the environment variable NATS_NKEY to use challenge response authentication by setting a file containing your private key.\n"
+            + "\nSet the environment variable NATS_CREDS to use JWT/NKey authentication by setting a file containing your user creds.\n"
+            + "\nUse the URL for user/pass/token authentication.\n";
 
     public static void main(String[] args) {
-        ExampleArgs exArgs = ExampleArgs.builder()
-                .defaultStream("example-stream")
-                .defaultSubject("example-subject")
-                .defaultMessage("hello")
-                .defaultMsgCount(10)
-                .build(args, usageString);
+        ExampleArgs exArgs = ExampleArgs.builder().defaultStream("example-stream").defaultSubject("example-subject")
+                .defaultMessage("hello").defaultMsgCount(10).build(args, usageString);
 
         String hdrNote = exArgs.hasHeaders() ? ", with " + exArgs.headers.size() + " header(s)" : "";
         System.out.printf("\nPublishing to %s%s. Server is %s\n\n", exArgs.subject, hdrNote, exArgs.server);
 
         try (Connection nc = Nats.connect(ExampleUtils.createExampleOptions(exArgs.server))) {
 
-            // Create a JetStream context.  This hangs off the original connection
+            // Create a JetStream context. This hangs off the original connection
             // allowing us to produce data to streams and consume data from
             // JetStream consumers.
             JetStream js = nc.jetStream();
@@ -66,17 +55,21 @@ public class NatsJsPub {
             NatsJsUtils.createOrUpdateStream(nc, exArgs.stream, exArgs.subject);
 
             int stop = exArgs.msgCount < 2 ? 2 : exArgs.msgCount + 1;
+            Message msg = NatsMessage.builder().subject(exArgs.subject).headers(exArgs.headers)
+                    // .data(data, StandardCharsets.UTF_8)
+                    .data(new byte[exArgs.msgLength]).build();
             for (int x = 1; x < stop; x++) {
                 // make unique message data if you want more than 1 message
-                String data = exArgs.msgCount < 2 ? exArgs.message : exArgs.message + "-" + x;
+                // String data = exArgs.msgCount < 2 ? exArgs.message : exArgs.message + "-" +
+                // x;
 
                 // create a typical NATS message
-                Message msg = NatsMessage.builder()
-                        .subject(exArgs.subject)
-                        .headers(exArgs.headers)
-                        // .data(data, StandardCharsets.UTF_8)
-                        .data(new byte[exArgs.msgLength])
-                        .build();
+                // Message msg = NatsMessage.builder()
+                // .subject(exArgs.subject)
+                // .headers(exArgs.headers)
+                // // .data(data, StandardCharsets.UTF_8)
+                // .data(new byte[exArgs.msgLength])
+                // .build();
 
                 // Publish a message and print the results of the publish acknowledgement.
                 // We'll use the defaults for this simple example, but there are options
@@ -84,11 +77,11 @@ public class NatsJsPub {
                 // more. See the NatsJsPubWithOptionsUseCases.java example for details.
                 // An exception will be thrown if there is a failure.
                 PublishAck pa = js.publish(msg);
-                System.out.printf("Published message %s on subject %s, stream %s, seqno %d.\n",
-                       data, exArgs.subject, pa.getStream(), pa.getSeqno());
+                // System.out.printf("Published message %s on subject %s, stream %s, seqno
+                // %d.\n", data, exArgs.subject,
+                System.out.printf("Published message , seqno %d.\n", pa.getSeqno());
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
